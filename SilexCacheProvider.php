405b0b209b9173ce02f5a6984556203053582654
Svyatoslav Sobol <svsool@bk.ru>
2016-11-19 17:06:26 +0300

    silex 2

--- SilexCacheProvider.php
+++ SilexCacheProvider.php
@@ -4,19 +4,21 @@ namespace Beryllium\SilexCacheProvider;
 
 use Beryllium\Cache\Client\APCClient;
 use Beryllium\Cache\Client\MemcacheClient;
+use Pimple\Container;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Silex\Api\BootableProviderInterface;
+use Pimple\ServiceProviderInterface;
 use Beryllium\Cache\Client\FilecacheClient;
 use Beryllium\Cache\Statistics\Tracker\FilecacheStatisticsTracker;
 use Beryllium\Cache\Cache;
 
-class SilexCacheProvider implements ServiceProviderInterface
+class SilexCacheProvider implements BootableProviderInterface, ServiceProviderInterface
 {
     public function boot(Application $app)
     {
     }
 
-    public function register(Application $app)
+    public function register(Container $app)
     {
         // defaults (these can be overridden in the container)
         $app['be_cache.type']   = 'memcache';
@@ -26,48 +28,44 @@ class SilexCacheProvider implements ServiceProviderInterface
         $app['be_cache.prefix'] = '';
 
         // register the client
-        $app['be_cache.client'] = $app->share(
-            function () use ($app) {
-                $client = null;
+        $app['be_cache.client'] = function () use ($app) {
+            $client = null;
 
-                switch (strtolower($app['be_cache.type'])) {
-                    default:
-                        throw new \Exception('Invalid be_cache.type');
-                        break;
-                    case 'file':
-                    case 'filecache':
-                        if (!$app['be_cache.path'] || !is_writable($app['be_cache.path'])) {
-                            throw new \Exception('$app[\'be_cache.path\'] must be writable.');
-                        }
-                        $client = new FilecacheClient($app['be_cache.path']);
-                        if ($app['be_cache.stats']) {
-                            $stats = new FilecacheStatisticsTracker($app['be_cache.path']);
-                            $client->setStatisticsTracker($stats);
-                        }
-                        break;
-                    case 'memcache':
-                        $memcache = new \Memcache();
-                        $memcache->addserver($app['be_cache.host'], $app['be_cache.port']);
-                        $client = new MemcacheClient($memcache);
-                        break;
-                    case 'apc':
-                        $client = new APCClient();
-                        break;
-                }
-
-                return $client;
+            switch (strtolower($app['be_cache.type'])) {
+                default:
+                    throw new \Exception('Invalid be_cache.type');
+                    break;
+                case 'file':
+                case 'filecache':
+                    if (!$app['be_cache.path'] || !is_writable($app['be_cache.path'])) {
+                        throw new \Exception('$app[\'be_cache.path\'] must be writable.');
+                    }
+                    $client = new FilecacheClient($app['be_cache.path']);
+                    if ($app['be_cache.stats']) {
+                        $stats = new FilecacheStatisticsTracker($app['be_cache.path']);
+                        $client->setStatisticsTracker($stats);
+                    }
+                    break;
+                case 'memcache':
+                    $memcache = new \Memcache();
+                    $memcache->addserver($app['be_cache.host'], $app['be_cache.port']);
+                    $client = new MemcacheClient($memcache);
+                    break;
+                case 'apc':
+                    $client = new APCClient();
+                    break;
             }
-        );
+
+            return $client;
+        };
 
         //register the service
-        $app['be_cache'] = $app->share(
-            function () use ($app) {
-                $cache = new Cache($app['be_cache.client']);
-                $cache->setPrefix($app['be_cache.prefix']);
+        $app['be_cache'] = function () use ($app) {
+            $cache = new Cache($app['be_cache.client']);
+            $cache->setPrefix($app['be_cache.prefix']);
 
-                return $cache;
-            }
-        );
+            return $cache;
+        };
     }
 
 } 
